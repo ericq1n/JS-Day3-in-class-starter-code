95469a4d7d8492aea7252b9695f8d966e7ef56e0
ericq1n <ericq1357@gmail.com>
2026-02-05 12:17:31 -0600

    part 1

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const members = [
 
 
 //OLD WAY DEMO - CONSTRUCTOR FUNCTION
+/*
 function Employee(firstName, lastName, email, birthdate, salary) {
     this.firstName = firstName;
     this.lastName = lastName;
@@ -27,7 +28,7 @@ function Employee(firstName, lastName, email, birthdate, salary) {
   console.log(bill);
 
   bill.editEmployee({ salary: 7777777, email: "xxxxxxx@example.com" });
-  console.log(bill);
+  console.log(bill);*/
 
 
 //ES6 way - CLASSES - Create a new Employee class that adds a new employee and console logs them
@@ -41,15 +42,64 @@ function Employee(firstName, lastName, email, birthdate, salary) {
 //    Test your methods using JS
 // 6. Try to get instances of your class object to display in the table.  You can set the innerhtml of the
 //    of the table to be empty and then replace it with the looped-through values of your object
+class Employee {
+  constructor(firstName, lastName, email, birthdate, salary) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+    this.birthdate = birthdate;
+    this.salary = salary;
+  }
 
+  static addEmployee(firstName, lastName, email, birthdate, salary) {
+    return new Employee(firstName, lastName, email, birthdate, salary);
+  }
+
+  static getEmployees() {
+    return(
+      {
+      firstName: this.firstName,
+      lastName: this.lastName,
+      email: this.email,
+      birthdate: this.birthdate,
+      salary: this.salary,
+      }
+  )}
+
+  editEmployee(updates) {
+    Object.assign(this, updates);
+  }
+}
 
 //Try to output 3 instances of your class object into the table
+const eric = Employee.addEmployee('Eric', 'Qin', 'eq998@utexas.edu', '2004-09-05', 100000);
+console.log(eric);
+console.log(eric.firstName);
 
+const empArray = [
+  Employee.addEmployee('Eric', 'Qin', 'eq998@utexas.edu', '2004-09-05', 100000),
+  Employee.addEmployee(members[0].first_name, members[0].last_name, members[0].email, members[0].birthdate, members[0].salary),
+  Employee.addEmployee(members[1].first_name, members[1].last_name, members[1].email, members[1].birthdate, members[1].salary)
+];
+console.log(empArray);
 
 
 
 // PART B Why we can't us synchrnous code
- 
+ function getTodosSynchronous() {     
+  console.log("1. Starting to fetch todos...");          
+  const reply =  fetch('https://jsonplaceholder.typicode.com/todos');     
+  console.log("2. Response:", reply);          
+  console.log("3. This runs immediately, BEFORE data arrives!");          
+  return reply; 
+}  
+
+const result = getTodosSynchronous(); 
+
+console.log("4. Result:", result); 
+
+console.log("5. We can't access the actual todo data this way!");
+
 
 
 
